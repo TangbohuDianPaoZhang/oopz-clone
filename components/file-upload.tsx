e57b8da74bf4a34c9bffb3092b602ed225747122
TangbohuDPZ <93420604+TangbohuDianPaoZhang@users.noreply.github.com>
2026-07-21 00:16:57 +0800

    fix the update for uploadthing, make file name can be rendered

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,12 +1,13 @@
 "use client";
 
+import { useState } from "react";
 import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 
 import { UploadDropzone } from "@/lib/uploadthing";
 
 interface FileUploadProps {
-  onChange: (url?: String) => void;
+  onChange: (url?: string) => void;
   value: string;
   endpoint: "messageFile" | "serverImage";
 }
@@ -16,8 +17,15 @@ export const FileUpload = ({
   value,
   endpoint
 }: FileUploadProps) => {
-  const fileType = value?.split(".").pop();
-  if (value && fileType !== "pdf") {
+
+  const [fileType, setFileType] = useState<string>("");
+  const [fileName, setFileName] = useState("");
+
+  const isImage = fileType.startsWith("image/");
+  const isPdf = fileType === "application/pdf";
+
+
+  if (value && isImage) {
     return (
       <div className="relative h-20 w-20">
         <Image
@@ -26,50 +34,67 @@ export const FileUpload = ({
           alt="Upload"
           className="rounded-full"
         />
+
         <button
-          onClick={() => onChange("")}
+          onClick={() => {
+            onChange("");
+            setFileType("");
+          }}
           className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
           type="button"
         >
           <X className="h-4 w-4" />
         </button>
       </div>
-    )
+    );
   }
 
-  if (value && fileType === "pdf") {
+
+  if (value && isPdf) {
     return (
       <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
         <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
-        <a 
+        <a
           href={value}
           target="_blank"
           rel="noopener noreferrer"
           className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
         >
-          {value}
+          {fileName}
         </a>
         <button
-          onClick={() => onChange("")}
-          className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
+          onClick={() => {
+            onChange("");
+          }}
+          className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2"
           type="button"
         >
           <X className="h-4 w-4" />
         </button>
+
       </div>
-    )
+    );
   }
+
 
   return (
     <UploadDropzone
       endpoint={endpoint}
       onClientUploadComplete={(res) => {
-        onChange(res?.[0].ufsUrl);
-        console.log("Files: ", res)
+        const file = res?.[0];
+
+        if (!file) return;
+
+        onChange(file.ufsUrl);
+        setFileType(file.type);
+        setFileName(file.name);
+
+        console.log(file);
       }}
-      onUploadError={(error: Error) => {
+
+      onUploadError={(error) => {
         console.log(error);
       }}
     />
-  )
-}
+  );
+};
